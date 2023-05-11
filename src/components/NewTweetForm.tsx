ac60dbe4067b4c59d87fd2a52aced0aa5fca0523
isa-ismail <fahimismail75.scc@gmail.com>
2023-05-11 21:14:38 +0600

    mutate

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -2,21 +2,46 @@ import React from 'react'
 import Button from './Button'
 import ProfileImage from './ProfileImage'
 import { useSession } from 'next-auth/react'
+import { api } from '~/utils/api'
 
 const NewTweetForm = () => {
 
     const session = useSession()
 
-    if (session.status !== 'authenticated') return <></>
+    const [inputValue, setInputValue] = React.useState('')
 
     const user = session.data?.user
+
+    const [height, setHeight] = React.useState("0")
+
+    const createTweet = api.tweet.create.useMutation({
+        onSuccess: (newTweet) => {
+            console.log(newTweet)
+        }
+    })
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        createTweet.mutate({ content: inputValue })
+        setInputValue('')
+    }
+
+    if (session.status !== 'authenticated') return <></>
+
     return (
-        <form className='flex flex-col gap-2 border-b px-4 py-2'>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-2 border-b px-4 py-2'>
             <div className='flex gap-4'>
                 <ProfileImage src={user?.image} />
                 <textarea
-                    className='flex flex-grow resize-none overflow-hidden
-                     p-4 text-lg outline-none'
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter' && !e.shiftKey) {
+                            setHeight('1')
+                        }
+                    }}
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    className={`flex flex-grow resize-none overflow-hidden
+                     p-4 text-lg outline-none ${height === '0' ? 'h-12' : 'h-80'}`}
                     placeholder="What's happenning?"
                 />
             </div>
--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -6,14 +6,12 @@ import {
   protectedProcedure,
 } from "~/server/api/trpc";
 
-export const exampleRouter = createTRPCRouter({
-  // hello: publicProcedure
-  //   .input(z.object({ text: z.string() }))
-  //   .query(({ input }) => {
-  //     return {
-  //       greeting: `Hello ${input.text}`,
-  //     };
-  //   }),
+export const tweetRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(z.object({ content: z.string() }))
+    .mutation(async({ input: {content}, ctx }) => { 
+      return await ctx.prisma.tweet.create ({ data: { content, userId: ctx.session.user.id }})
+     }),
 
   // getAll: publicProcedure.query(({ ctx }) => {
   //   return ctx.prisma.example.findMany();
